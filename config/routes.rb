309cdf15e5ce86c4cc5300c1510ab2cdfc6c21a4
ELIYA NETANEL <eliya313@gmail.com>
2018-08-06 15:00:13 +0300

    works, dry

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #create
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create', as: :tasks
+  # #read
+  #   #index
+  # get 'tasks', to: 'tasks#index' # as: :tasks
+  #   #show
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # #update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # #delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  # #homepage
+  root to: 'tasks#index'
+  resources :tasks
 end
